cfbedf50f2185b6a851c150db635d58835ce4482
kAikvvf <157582610+kAikvvf@users.noreply.github.com>
2025-05-24 19:23:02 +0900

    modified

--- debugFile.py
+++ debugFile.py
@@ -1,7 +1,4 @@
 from libraries.caseVerificator import getCaseInput
 def run():
-    x = getCaseInput(0,0,"first")
-    y = getCaseInput(0,1,"second")
-    z = getCaseInput(0,2,"third")
-    print(x+y+z)
-    print(x,y,z)
+    x = getCaseInput(0,0,"any")
+    print(x)
