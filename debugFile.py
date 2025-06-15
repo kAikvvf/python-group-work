aa43f0f2dd505af3f23750a46a28712e5d17fa68
kAikvvf <157582610+kAikvvf@users.noreply.github.com>
2025-06-15 17:42:48 +0900

    rewrited entire progs of prompt

--- debugFile.py
+++
@@ -1,4 +0,0 @@
-from libraries.caseVerificator import getCaseInput
-def run():
-    x = int(4.10289)
-    print(x)
--- scripts/prompt.py
+++ scripts/prompt.py
@@ -1,56 +1,35 @@
-from libraries.codeDebugger import codeDebugger
-import tkinter as tk
+from tkinter import *
+import importlib
 import math
+import data.questDataExtracter as dataExtracter
 
-class prompt:
-    def __init__(self,
-                 root_Tk,
-                 input_font_size = 5,
-                 ):
-        self.Prompt = tk.Canvas(master=root_Tk) #プロンプト全体のエリア
-        Button_Pad = tk.Canvas(master=self.Prompt) #ボタンを表示するエリア
+class editor:
+    def __init__(self,master,question_number): #プログラム番号で呼び出し。インデックスではない。
+        self.root = master
+        self.prog_index = question_number
+        self.result = {'result':'null','matches':'null'} #結果を格納
 
-        #コマンドを入力するプロンプトのウィジェット
-        self.input_pad = tk.Text(self.Prompt,
-                                 font=("Bahnschrift SemiCondensed",input_font_size),
-                                 foreground="#FFFFFF",
-                                 background="#2D2D2D")
-        
-        #サンプル実行ボタン
-        test_debug_button = tk.Button(Button_Pad,
-                                      text="サンプル実行",
-                                      command=self.test_debug,
-                                      font=("BIZ UDゴシック",10,"bold"),
-                                      background="#69B7FC",
-                                      relief="groove")
+        #Tkinter の設定
+        self.editor_frame = Canvas(master=self.root)
+        self.type_area = Text(master=self.editor_frame,background="#4A4A4A",foreground="#9EFFFF",font=("Source Han Code JP",11,"bold"))
+        self.editor_frame.place(relheight=1.0,relwidth=0.5,relx=0.5,rely=0.0)
+        button_area = Frame(master=self.editor_frame)
+        sample_debug_button = Button(master=button_area,text="サンプル実行",command=self.sampleDebug)
+        sample_debug_button.grid(column=0,row=0)
+        scoring_button = Button(master=button_area,text="採点",command=self.scoreing)
+        scoring_button.grid(column=1,row=0)
+        button_area.pack(side=BOTTOM)
+        self.type_area.pack(fill='both',expand=1)
 
-        #採点ボタン
-        confirm_answer_button = tk.Button(Button_Pad,
-                                          text="採点",
-                                          command=self.confirm_answer,
-                                          font=("BIZ UDゴシック",10,"bold"),
-                                          background="#FF6FBC",
-                                          relief="groove")
-
-        #各コンポーネントを配置
-        test_debug_button.pack(anchor=tk.SE,side=tk.LEFT,padx=5,pady=3,ipadx=3,ipady=3)
-        confirm_answer_button.pack(anchor=tk.SE,side=tk.LEFT,padx=5,pady=3,ipadx=3,ipady=3)
-        Button_Pad.pack(anchor=tk.SE,side=tk.BOTTOM)
-        self.input_pad.pack(fill=tk.BOTH,expand=1)
-
-        #----------編集時に書きやすくするコマンド----------#
-        self.indent = 0
-        self.index = 0
-
-        #__改行時にインデントをそろえる__
+        #インデントをそろえる
         def arrangeIndent(event):
-            index_now = self.input_pad.index('insert')
-            index_inline_now = self.input_pad.index('insert lineend')
+            index_now = self.type_area.index('insert')
+            index_inline_now = self.type_area.index('insert lineend')
 
             row_now = math.floor(float(index_now))
-            content_in_line = self.input_pad.get(f'{row_now}.0',f'{row_now}.end')
+            content_in_line = self.type_area.get(f'{row_now}.0',f'{row_now}.end')
             
-            self.input_pad.insert('insert',"\n")
+            self.type_area.insert('insert',"\n")
             if content_in_line != "":
                 #インデントのスペースを数える
                 index = 0
@@ -67,65 +46,78 @@ class prompt:
 
                     if indent > 0 & index % 4 ==0:
                         for i in range(indent):
-                            self.input_pad.insert('insert',"    ")     
+                            self.type_area.insert('insert',"    ")     
             return "break"
 
         #インデントを Tab で入れる
         def tabKey(event):
-            self.input_pad.insert('insert',"    ")
+            self.type_area.insert('insert',"    ")
             return 'break'
         
         def shiftTabKey(event):
-            if self.input_pad.get('insert -4c','insert') == "    ":
-                self.input_pad.delete('insert -4c','insert')
+            if self.type_area.get('insert -4c','insert') == "    ":
+                self.type_area.delete('insert -4c','insert')
             return 'break'
 
         #__実行部分__
-        self.input_pad.configure(insertbackground="#FFFFFF",insertwidth=3)
-        self.input_pad.bind('<Return>',arrangeIndent)
-        self.input_pad.bind('<Tab>',tabKey)
-        self.input_pad.bind('<Shift-Tab>',shiftTabKey)
+        self.type_area.configure(insertbackground="#FFFFFF",insertwidth=3)
+        self.type_area.bind('<Return>',arrangeIndent)
+        self.type_area.bind('<Tab>',tabKey)
+        self.type_area.bind('<Shift-Tab>',shiftTabKey)
 
-        #--------------------#
-    
-    #サンプル実行
-    def test_debug(self):
-        print("sample")
-        print(self.debug())
-    
-    #採点する
-    def confirm_answer(self):
-        print("confirm")
-        print(self.debug())
+    def sampleDebug(self):
+        for i in range(int(dataExtracter.numberOfCases(quest_index=self.prog_index))):
+            print(f"sample {i}\n",self.debug(i))
 
-    #デバッグする
-    def debug(self):
-        prog = self.input_pad.get("1.0","end").splitlines("\n")
+    def scoreing(self):
+        print("scoring",self.debug(0))
+
+    def debug(self,case_index):
+        sample_case = dataExtracter.sampleCase(quest_index=self.prog_index,case_index=case_index)
+        prog = self.type_area.get("1.0","end").splitlines("\n")
         num_of_input = 0
-        task_num = 0
         for i in range(len(prog)):
             if "input(" in prog[i]:
-                prog[i] = prog[i].replace("input(",f"getCaseInput({task_num},{num_of_input},")
+                inputed_data = sample_case[1][num_of_input]
+                prog[i] = prog[i].replace("input(",f"inputMode({inputed_data},")
                 num_of_input += 1
             prog[i] = "    " + prog[i]
-        prog = ["from libraries.caseVerificator import getCaseInput\ndef run():\n"]+prog
-        print(prog)
-        debugger = codeDebugger
-        result = debugger.debug(prog=prog)
-        return(result)
+        prog = ["def inputMode(inputed_data,message):\n    print(message,inputed_data)\n    return(inputed_data)\ndef run():\n"]+prog+["def generateResult():\n    import sys\n    sys.stdout = open('scripts/debugTerminal.txt','w',encoding='utf-8')\n    try:\n        run()\n    except Exception as e:\n        print(e)\n    finally:\n        sys.stdout.close()\n        sys.stdout = sys.__stdout__"]
         
-    #デバッグの結果を取得
-    def get_result(self):
-        return(self.debug())
+        with open("scripts/run.py",'w',encoding="utf-8") as runFile:
+            runFile.write("".join(prog))
+        runFile.close()
+        
+        #実行ファイルをインポートして実行
+        debugProg = importlib.import_module("run")
+        importlib.reload(debugProg)
+        debugProg.generateResult()
+        
+        with open("scripts/debugTerminal.txt","r",encoding="utf-8") as debugResultFile:
+            self.degugResult = debugResultFile.read()
+            correct_answer = dataExtracter.sampleCase(quest_index=self.prog_index,case_index=case_index)[2]
+            if self.degugResult == correct_answer:
+                self.matches = True
+            else:
+                self.matches = False
+            self.result['result'] = self.degugResult
+            self.result['matches'] = self.matches
+            debugResultFile.close()
 
-    #プロンプトを表示
+        return(self.result)
+
+
     def show(self):
-        self.Prompt.place(relheight=1.0,relwidth=0.5,relx=0.5,rely=0.0)
+        self.editor_frame.place(relheight=1.0,relwidth=0.5,relx=0.5,rely=0.0)
 
-    #プロンプトを非表示
     def hide(self):
-        self.Prompt.place_forget()
-    
-    #サンプルケースを設定
-    def setInputSampleCase(self,cases):
-        self.input_cases = cases
+        self.editor_frame.place_forget()
+
+root = Tk()
+root.geometry("800x500")
+editor1 = editor(root,1)
+
+quest_statement = Label(master=root,text=dataExtracter.questStatement(quest_index=1))
+quest_statement.pack(side="left")
+
+root.mainloop()
