7a7732726a3548de896fd142f65980973b00753d
kAikvvf <157582610+kAikvvf@users.noreply.github.com>
2025-07-15 22:23:40 +0900

    made first quest

--- scripts/run_temp/runProg1.py
+++ scripts/run_temp/runProg1.py
@@ -3,5 +3,5 @@ def inputMode(inputed_data,*message):
         print(message[0])
     return(inputed_data)
 def generateResult():
-    print(inputMode(21,'好きな整数を入力してください。'))
+    print(inputMode(28,'好きな整数を入力してください。'))
     
